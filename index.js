0bda5edfaf9f47fe0b2f500a884a297ab4c71406
nazarevitsch <bidaritter1997@gmail.com>
2020-06-02 18:18:41 +0300

    Delete all unnecessary comments

--- index.js
+++ index.js
@@ -8,11 +8,3 @@ server.use(routes);
 server.listen(8099, () =>{
     console.log("Server started on port 8090...")
 });
-
-
-// const server = http.createServer( (req, res) => {
-//     console.log("PIDER ", req.url, " Count: ", counter++);
-//     if (req.url == "/create_order") {
-//         res.statusCode = 200;
-//         res.end("OK");
-//     }
